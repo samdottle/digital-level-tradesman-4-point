7f9fc88bc04fa4c1c7b76c6152ff96babe85f9c5
samdottle <66985382+samdottle@users.noreply.github.com>
2026-07-18 11:29:30 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dlt4-v1.2';
+const CACHE='dlt4-v1.0';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
